47d52cbb7a6efea125d7afb3021a94fa5e3626ab
liupeining <56020224+liupeining@users.noreply.github.com>
2020-08-17 20:25:03 +0800

    Add files via upload

---
+++ LookForBall.h
@@ -0,0 +1,50 @@
+/** This option lets the robot look for the ball. */
+option(LookForBall, (float)(0.38f) tilt, (float)(0.5f) pan)
+{
+  initial_state(lookRight)
+  {
+    transition
+    {
+       if(action_done)    
+          goto lookLeft;
+    }
+    action
+    {
+      Stand();
+      SetHeadPanTilt(-pan, tilt, 45_deg);
+      
+    }
+  }
+  
+   state(lookLeft)
+  {
+    transition
+    {
+        
+      if(action_done){
+          HeadControlMode(HeadControl::lookForward);
+          goto moveBody;
+      }    
+          
+    }
+    action
+    {
+      SetHeadPanTilt(pan, tilt, 45_deg);
+    }
+  }
+  
+  state(moveBody){
+      transition{
+              
+          if(state_time > 2000) 
+              goto lookRight;
+      }
+      action{
+          
+          HeadControlMode(HeadControl::lookForward);
+          WalkAtRelativeSpeed(Pose2f(1.f, 0.f, 0.f));
+          
+      }
+  }
+
+}
